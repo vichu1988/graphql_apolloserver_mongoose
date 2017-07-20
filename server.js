753e40e15ba928a171a9940aecbb984352d510bf
Visweshwaran S <vsuryanarayanan@sapient.com>
2017-07-20 19:42:47 +0530

    changes

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import Cors from 'cors';
 import express from 'express';
-import { apolloServer } from 'graphql-tools';
-import Resolvers from './data/resolvers';
+//import { apolloServer } from 'graphql-tools';
+import { graphqlExpress } from 'apollo-server-express';
 import Schema from './data/schema';
+
 const GRAPHQL_PORT = 8080;
 var graphQLServer = express();
 graphQLServer.use(Cors());
-graphQLServer.use('/graphql', apolloServer({
-  graphiql : true,
-  pretty   : true,
-  schema   : Schema,
-  resolvers: Resolvers,
+graphQLServer.use('/graphql', graphqlExpress({
+  schema: Schema
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
